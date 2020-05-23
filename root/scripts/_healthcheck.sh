5604ee0e1bbee02ffa86aefb8632a2e15f2a7236
Sasa Marjancic <s.marjancic@gmail.com>
2020-05-23 17:02:34 +0200

    Update _healthcheck.sh

--- root/scripts/_healthcheck.sh
+++ root/scripts/_healthcheck.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bash
 
-status_code=$(curl -s -o /dev/null -i -w "%{http_code}" $2)
+status_code=$(curl -Ls -o /dev/null -i -w "%{http_code}" $2)
 if [[ "$status_code" -ne 200 ]] ; then
   curl -s --retry 3 https://hc-ping.com/$1/fail > /dev/null
 else
