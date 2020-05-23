924e301008cd24f65a2d8f18d14f2162249aa930
Sasa Marjancic <s.marjancic@gmail.com>
2020-05-23 16:41:37 +0200

    Update run_healthcheck.sh

--- root/scripts/run_healthcheck.sh
+++ root/scripts/run_healthcheck.sh
@@ -5,6 +5,6 @@ declare -A hashmap
 while IFS="=" read -r key value
 do
     hashmap[$key]="$value"
-done < <(jq -r 'to_entries|map("(.key)=(.value)")|.[]' /config/healthcheck.json)
+done < <(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' /config/healthcheck.json)
 
 for key in "${!hashmap[@]}"; do /scripts/_healthcheck.sh $key "${hashmap[$key]}"; done
