c07d2c1247bde8a14f2a6ab218aece128c3c08a9
Sasa Marjancic <s.marjancic@gmail.com>
2020-05-23 15:24:56 +0200

    Update run_healthcheck.sh

--- root/scripts/run_healthcheck.sh
+++ root/scripts/run_healthcheck.sh
@@ -7,7 +7,4 @@ do
     hashmap[$key]="$value"
 done < <(jq -r 'to_entries|map("(.key)=(.value)")|.[]' /config/healthcheck.json)
 
-# Pi Hole
-hashmap["0626ec70-1a2b-4e93-8f26-444b6b70f521"]="http://192.168.90.220:80"
-
 for key in "${!hashmap[@]}"; do /scripts/_healthcheck.sh $key "${hashmap[$key]}"; done
