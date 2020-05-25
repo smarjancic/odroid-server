93a71c437335a34fc50bcbf54a99905dece20163
Sasa Marjancic <s.marjancic@gmail.com>
2020-05-25 16:25:09 +0200

    healthcheck refactor

--- root/scripts/run_healthcheck.sh
+++ root/scripts/run_healthcheck.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/with-contenv bash
 
+do_healthcheck() {
+  if  [[ $2 = http* ]] ; then
+    status_code=$(curl -Ls -o /dev/null -i -w "%{http_code}" $2)
+  else
+    if  ping -q -c 1 $2 2>&1 >/dev/null ; then
+      status_code=200
+    fi
+  fi
+  if [[ "$status_code" -ne 200 ]] ; then
+    curl -s --retry 3 https://hc-ping.com/$1/fail > /dev/null
+  else
+    curl -s --retry 3 https://hc-ping.com/$1 > /dev/null
+  fi
+}
+
 declare -A hashmap
 
 while IFS="=" read -r key value
@@ -7,4 +22,4 @@ do
     hashmap[$key]="$value"
 done < <(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' /config/healthcheck.json)
 
-for key in "${!hashmap[@]}"; do /scripts/_healthcheck.sh $key "${hashmap[$key]}"; done
+for key in "${!hashmap[@]}"; do do_healthcheck $key "${hashmap[$key]}"; done
